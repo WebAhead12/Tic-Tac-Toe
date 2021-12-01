a1c73c1ff12a1867a8a45c0847094d1d8b9851a7
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2021-12-01 10:24:42 +0200

    CSS changes + added register route

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import Login from "./Components/Login/login";
+import RegisterPage from "./Components/Register/register";
 import "./App.css";
 import Game from "./Components/Game/MainGame";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -14,7 +15,8 @@ const App = () => {
       {/* Routes makes sure that only one path opens at a time */}
 
       <Routes>
-        <Route path="/user/login" element={<Login />} />
+        <Route path="/users/login" element={<Login />} />
+        <Route path="/users/register" element={<RegisterPage />} />
 
         {/* the "exact" makes the path match only "/" and not also "/user/login" */}
         <Route exact path="/" element={<Game />} />
